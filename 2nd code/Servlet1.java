099576bcfa19a9ccf9477319fafa89212fd981cc
CIJU <151955625+ChrisilDavid28@users.noreply.github.com>
2024-11-03 22:51:51 +0530

    Update Servlet1.java

--- 2nd code/Servlet1.java
+++ 2nd code/Servlet1.java
@@ -1,27 +1,29 @@
 //package com.example.demo;
 
-import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-@WebServlet("/Servlet1")
-public class Servlet1 extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-        // You can add some input form here or any other content
-        out.print("<h1>Welcome to Servlet1</h1>");
-        out.print("<form action='Servlet2' method='get'>");
-        out.print("Name: <input type='text' name='username' required><br>");
-        out.print("City: <input type='text' name='usercity' required><br>");
-        out.print("<input type='submit' value='Submit'>");
-        out.print("</form>");
-        out.close();
-    }
+public class Servlet1 extends HttpServlet
+{
+public void doGet(HttpServletRequest request,HttpServletResponse response)
+{
+try
+{
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+String n=request.getParameter("uname");
+String c=request.getParameter("ucity");
+out.print("<form action=\"Servlet2\">");
+out.print("<input type='hidden' name='uname' value="+n+">");
+out.print("<input type='hidden' name='ucity' value="+c+">");
+out.print("<input type='submit' value='Just click here'>");
+out.print("</form>");
+out.close();
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
 }
