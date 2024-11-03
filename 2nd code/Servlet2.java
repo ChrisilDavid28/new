d9ed0532f82c64068b64b7eca8e063ab7b97c1b1
CIJU <151955625+ChrisilDavid28@users.noreply.github.com>
2024-11-03 22:52:16 +0530

    Update Servlet2.java

--- 2nd code/Servlet2.java
+++ 2nd code/Servlet2.java
@@ -1,28 +1,20 @@
-package com.example.demo;
+//package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import java.io.*;
+import jakarta.servlet.http.*;
 
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+public class Servlet2 extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            String n = request.getParameter("uname");
+            out.println("Hello " + n);
+            String c = request.getParameter("ucity");
+            out.print("you are from " + c);
+            out.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-
-    @Bean
-    public ServletRegistrationBean<Servlet1> servlet1Registration() {
-        ServletRegistrationBean<Servlet1> registration = new ServletRegistrationBean<>(new Servlet1(), "/Servlet1");
-        registration.setName("Servlet1");
-        return registration;
-    }
-
-    @Bean
-public ServletRegistrationBean<Servlet2> servlet2Registration() {
-    ServletRegistrationBean<Servlet2> registration = new ServletRegistrationBean<>(new Servlet2(), "/Servlet2");
-    registration.setName("Servlet2");
-    return registration;
-}
-
 }
